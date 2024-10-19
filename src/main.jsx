9332581ec9700c39cbe7249769afbb66b33b4c5d
Cedric Ko <yacmov@gmail.com>
2024-10-19 22:21:31 +0800

    feat(main): setup main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+// Import essential CSS styles
+import './assets/css/bootstrap.min.css';
+import './assets/css/style.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
