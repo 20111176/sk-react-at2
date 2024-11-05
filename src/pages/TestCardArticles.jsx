21d1c6b812e316de333fae8e16a1acafb6ceb773
Cedric Ko <yacmov@gmail.com>
2024-11-06 00:09:45 +0800

    fix(TestCardArticles): appled props as updated component

--- src/pages/TestCardArticles.jsx
+++ src/pages/TestCardArticles.jsx
@@ -1,7 +1,29 @@
 import CardArticles from '../components/ContentWrapper/CardArticles';
-
+const articlesData = [
+  {
+    date: 'July 30, 2014',
+    comments: 2,
+    tags: ['Properties, ', 'Prices, ', 'best deals'],
+    title: 'How to get your dream property for the best price?',
+    body: 'Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.',
+  },
+  {
+    date: 'July 24, 2014',
+    comments: 4,
+    tags: ['Tips, ', 'Mortgage'],
+    title: '7 tips to get the best mortgage.',
+    body: 'Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.',
+  },
+  {
+    date: 'July 05, 2014',
+    comments: 1,
+    tags: ['Location, ', 'Price, ', 'House'],
+    title: "House, location or price: What's the most important factor?",
+    body: 'Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.',
+  },
+];
 const TestCardArticles = () => {
-  return <CardArticles />;
+  return <CardArticles title='test article page' data={articlesData} />;
 };
 
 export default TestCardArticles;
